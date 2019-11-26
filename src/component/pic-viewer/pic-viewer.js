95455e9f51b42129051883762f4da4ae6ae8fa38
djjiang <1561624238@qq.com>
2019-11-26 20:46:59 +0800

    修改代码

--- src/component/pic-viewer/pic-viewer.js
+++ src/component/pic-viewer/pic-viewer.js
@@ -18,9 +18,18 @@ class PicViewer extends Component {
     super(props)
     this.state = {
       hammerList: [],
+      initScale: 1,
+      tMatrix: [1,0,0,1,0,0], //x缩放，无，无，y缩放，x平移，y平移
+      poscenter: this.point2D(0, 0), //缓存双指的中心坐标
+      lastTranslate: this.point2D(0, 0), //记录上次的偏移值
+      lastcenter: this.point2D(0, 0), //图像的中心点，用于对比双指中心点
+      center: this.point2D(0, 0),
+      duration: ""
     }
   }
-
+  point2D(x, y) {
+    return {x: x, y: y}
+  }
   componentDidMount() {
     // 初始化swiper
     new Swiper(ReactDOM.findDOMNode(this.swiper), {
@@ -31,27 +40,86 @@ class PicViewer extends Component {
     const hammerList = []
     Array.from(swiperSlide).forEach(slide => {
       const hammer = new Hammer(slide)
-      hammer.get('pan').set({ enable: true })
-      hammer.get('pinch').set({ enable: true })
+      hammer.get('pan').set({ enable: true,direction: Hammer.DIRECTION_ALL })
+      hammer.get('pinch').set({ enable: true,direction: Hammer.DIRECTION_ALL })
       hammerList.push(hammer)
       this.updateGestrueEvent(hammer)
+    })
+    this.setState({
+      center: this.point2D(swiperSlide[0].offsetWidth/2, swiperSlide[0].offsetHeight/2)
     })
   }
   // 监听当前dom的手势事件
   updateGestrueEvent(hammer) {
-    hammer.on('pinchstart pinchin pinchout', (ev) => {
-      const type = ev.type
-      const target = ev.target
-      let initScale = 1
-      if(type === 'pinchstart') {
-        initScale = getTransformAttr(getStyle(target, "transform"), "scale") || 1
-        console.log("initScale1", initScale)
-      }
-      if(type === 'pinchout') {
-        console.log("initScale2", initScale)
-        target.style.transform = `scale(${ev.scale * initScale})`
-      }
+    hammer.on('panstart', (ev) => {
+      this.setState({
+        lastTranslate: this.point2D(this.state.tMatrix[4], this.state.tMatrix[5]) //缓存上一次的偏移值
+      })
     })
+    hammer.on("panmove", (ev) => {
+      let tMatrix = this.state.tMatrix
+      const target = ev.target
+      tMatrix[4] = this.state.lastTranslate.x + ev.deltaX
+		  tMatrix[5] = this.state.lastTranslate.y + ev.deltaY
+      this.setState({
+        tMatrix: tMatrix,
+        duration: ""
+      }, () => {
+        target.style.transition = this.state.duration
+        target.style.transform = `matrix(${this.state.tMatrix.join(",")})`
+      })
+    })
+    hammer.on('pinchstart', (ev) => {
+      const initScale = this.state.tMatrix[0] || 1// 记录上一次的scale值
+      const lastTranslate = this.point2D(this.state.tMatrix[4], this.state.tMatrix[5]) // 记录上次的偏移值
+      let poscenter = this.point2D(ev.center.x, ev.center.y)
+      const lastcenter = this.point2D(this.state.center.x + lastTranslate.x, this.state.center.y + lastTranslate.y)//重新计算放大后的中心坐标
+      poscenter = this.point2D(ev.center.x - lastcenter.x, ev.center.y-lastcenter.y)
+      this.setState({
+        initScale: initScale, 
+        lastTranslate: lastTranslate, // 记录上次的偏移值
+        poscenter: poscenter,
+        lastcenter: lastcenter,
+        duration: ""
+      })
+    })
+    hammer.on('pinchmove', (ev) => {
+      let tMatrix = this.state.tMatrix
+      const target = ev.target
+      tMatrix[0] = tMatrix[3] = this.state.initScale * ev.scale
+      tMatrix[4] = (1 - ev.scale) * this.state.poscenter.x + this.state.lastTranslate.x
+      tMatrix[5] = (1 - ev.scale) * this.state.poscenter.y + this.state.lastTranslate.y
+      this.setState({
+        tMatrix: tMatrix,
+        duration: ""
+      }, () => {
+        target.style.transition = this.state.duration
+        target.style.transform = `matrix(${this.state.tMatrix.join(",")})`
+      })
+    })
+    hammer.on('panend', (ev) => {
+      this.endReset(ev)
+    })
+    hammer.on('pinchend', (ev) => {
+      this.endReset(ev)
+    })
+  }
+  endReset(ev){
+    let tMatrix = this.state.tMatrix
+    const target = ev.target
+    console.log(tMatrix.join(","))
+    if(tMatrix[0] <= 1) {
+      tMatrix[0] = tMatrix[3] = 1
+      tMatrix[4] = tMatrix[5] = 0
+      this.setState({
+        initScale: 1,
+        tMatrix: tMatrix,
+        duration: ".3s ease all"
+      }, () => {
+        target.style.transition = this.state.duration
+        target.style.transform = `matrix(${this.state.tMatrix.join(",")})`
+      })
+    }
   }
   // 关闭遮罩层
   handleCloseEvent() {
@@ -75,7 +143,7 @@ class PicViewer extends Component {
                 urlList.map((url, index) => {
                   return (
                     <div
-                      className={classNames("swiper-slide", { 'swiper-no-swiping': false })}
+                      className={classNames("swiper-slide", { 'swiper-no-swiping': true })}
                       key={index}
                     >
                       <img
